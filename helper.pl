b4cdad3af37dee100c0f6c062459ce8ca520d574
Alexander Panteleev <aleksandr.panteleev@domrf.ru>
2021-07-27 20:22:59 +0300

    [r] remove unnecessary switch tag

--- helper.pl
+++ helper.pl
@@ -1,7 +1,7 @@
-my ($helper) = @ARGV;
+my ($action) = @ARGV;
 
-SWITCH: for ($helper) {
-    if (/up/)    { exec('docker compose up -d');     last SWITCH; }
-    if (/down/)  { exec('docker compose down -t 5'); last SWITCH; }
-    if (/logs/)  { exec('docker compose logs -f');   last SWITCH; }
+for ($action) {
+    if (/up/)    { exec('docker compose up -d');     }
+    if (/down/)  { exec('docker compose down -t 5'); }
+    if (/logs/)  { exec('docker compose logs -f');   }
 }
