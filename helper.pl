f38ddd829faff16a312096db1ecb76869ccc854c
Alexander Panteleev <aleksandr.panteleev@domrf.ru>
2021-07-28 12:29:11 +0300

    [i] 1) optimized default.conf 2) add helper attach

--- helper.pl
+++ helper.pl
@@ -1,7 +1,10 @@
-my ($action) = @ARGV;
+my ($action, $arg1) = @ARGV;
 
 for ($action) {
-    if (/up/)    { exec('docker compose up -d');     }
-    if (/down/)  { exec('docker compose down -t 5'); }
-    if (/logs/)  { exec('docker compose logs -f');   }
+    if (/up/)      { exec('docker compose up -d');           }
+    if (/down/)    { exec('docker compose down -t 5');       }
+    if (/logs/)    { exec('docker compose logs -f');         }
+    if (/attach/)  { exec("docker compose exec $arg1 sh");   }
 }
+
+print 'Action not found';
