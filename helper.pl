3f13eaf963b4142317456be6099ebfba6aac1656
Alexander Panteleev <aleksandr.panteleev@domrf.ru>
2021-07-28 17:08:39 +0300

    [i] restart helper

--- helper.pl
+++ helper.pl
@@ -1,10 +1,11 @@
 my ($action, $arg1) = @ARGV;
 
 for ($action) {
-    if (/up/)      { exec('docker compose up -d');           }
-    if (/down/)    { exec('docker compose down -t 5');       }
-    if (/logs/)    { exec('docker compose logs -f');         }
+    if (/up/)      { exec("docker compose up -d");           }
+    if (/down/)    { exec("docker compose down -t 5");       }
+    if (/logs/)    { exec("docker compose logs -f");         }
     if (/attach/)  { exec("docker compose exec $arg1 sh");   }
+    if (/restart/)  { exec("docker compose restart");        }
 }
 
 print 'Action not found';
