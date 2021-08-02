d7a9443b110a18e9ae9dafaa3598aa4e9595d2c0
Alexander Panteleev <aleksandr.panteleev@domrf.ru>
2021-08-02 22:11:18 +0300

    [i] 1) db container 2) db helper

--- helper.pl
+++ helper.pl
@@ -1,11 +1,20 @@
+use Dotenv; 
+Dotenv->load;
+
 my ($action, $arg1) = @ARGV;
 
 for ($action) {
-    if (/up/)      { exec("docker compose up -d");           }
-    if (/down/)    { exec("docker compose down -t 5");       }
-    if (/logs/)    { exec("docker compose logs -f");         }
-    if (/attach/)  { exec("docker compose exec $arg1 sh");   }
-    if (/restart/)  { exec("docker compose restart");        }
+    # containers.
+    if (/up/)      { exec("docker compose up -d");         }
+    if (/down/)    { exec("docker compose down -t 5");     }
+    if (/logs/)    { exec("docker compose logs -f");       }
+    if (/attach/)  { exec("docker compose exec $arg1 sh"); }
+    if (/restart/) { exec("docker compose restart");       }
+
+    # db.
+    if (/db/)      { exec("docker compose exec db mysql -u$ENV{MYSQL_USER} -p$ENV{MYSQL_PASSWORD}"); }
 }
 
 print 'Action not found';
+
+__END__
