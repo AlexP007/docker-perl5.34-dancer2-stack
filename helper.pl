1a15906183fdc557d82613d8205f8a8710dc4ca0
Alexander Panteleev <aleksandr.panteleev@domrf.ru>
2021-08-04 20:28:15 +0300

    [i] cpanm container + helper

--- helper.pl
+++ helper.pl
@@ -8,6 +8,7 @@ for ($action) {
     if (/up/)      { exec("docker compose up -d");         }
     if (/down/)    { exec("docker compose down -t 5");     }
     if (/logs/)    { exec("docker compose logs -f");       }
+    if (/cpanm/)   { exec("docker compose run cpanm");     }
     if (/attach/)  { exec("docker compose exec $arg1 sh"); }
     if (/restart/) { exec("docker compose restart");       }
 
