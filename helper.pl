80e4bd107f3170ad52176b08548d8d64fe5d3d03
Alexander Panteleev <aleksandr.panteleev@domrf.ru>
2021-08-13 21:09:55 +0300

    [i] add --rm to cpanm helper command

--- helper.pl
+++ helper.pl
@@ -5,12 +5,12 @@ my ($action, $arg1, $arg2) = @ARGV;
 
 for ($action) {
     # containers.
-    if (/up/)      { exec("docker compose up -d");         }
-    if (/down/)    { exec("docker compose down -t 5");     }
-    if (/logs/)    { exec("docker compose logs -f");       }
-    if (/cpanm/)   { exec("docker compose run cpanm");     }
-    if (/attach/)  { exec("docker compose exec $arg1 sh"); }
-    if (/restart/) { exec("docker compose restart");       }
+    if (/up/)      { exec("docker compose up -d");          }
+    if (/down/)    { exec("docker compose down -t 5");      }
+    if (/logs/)    { exec("docker compose logs -f");        }
+    if (/cpanm/)   { exec("docker compose run --rm cpanm"); }
+    if (/attach/)  { exec("docker compose exec $arg1 sh");  }
+    if (/restart/) { exec("docker compose restart");        }
 
     # db.
     if (/db/)      { exec("docker compose exec db mysql -u$ENV{MYSQL_USER} -p$ENV{MYSQL_PASSWORD}"); }
